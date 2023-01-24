7324391ac11a13529a3cd452f7927a7ede772b58
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-01-24 22:59:09 +0200

    Shuffle cards

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ const cardContainerElem = document.querySelector('.card-container');
 const collapsedGridAreaTemplate = '"a a" "a a"';
 const cardCollectionCellClass = ".card-pos-a";
 
+const numCards = cardObjectDefinitions.length;
+
+let cardPositions = [];
+
 {/* <div class="card">
     <div class="card-inner">
         <div class="card-front">
@@ -84,6 +88,30 @@ function flipCards(flipToBack){
         }, index * 100);
     },)
 }
+function shuffleCards(){
+    const id = setInterval(shuffle, 12)
+    let shuffleCount = 0;
+
+    function shuffle() {
+        randomizeCardPositions();
+
+        if(shuffleCount == 500){
+            clearInterval(id);
+        }
+        else {
+            shuffleCount++;
+        }
+    }
+}   
+function randomizeCardPositions(){
+    const random1 = Math.floor(Math.random() * numCards) + 1
+    const random2 = Math.floor(Math.random() * numCards) + 1
+
+    const temp = cardPositions[random1 - 1];
+
+    cardPositions[random1 - 1] = cardPositions[random2 - 1];
+    cardPositions[random2 - 1] = temp;
+}
 
 function createCards(){
     cardObjectDefinitions.forEach((cardItem) => {
@@ -129,6 +157,11 @@ function createCard(cardItem){
     addChildElement(cardElem, cardInnerElem);
 
     addCardToGridCell(cardElem);
+
+    initializeCardPositions(cardElem);
+}
+function initializeCardPositions(card){
+    cardPositions.push(card.id);
 }
 
 function createElement(elemType){
